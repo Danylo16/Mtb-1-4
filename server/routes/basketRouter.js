48ba4cde4fc34f25aed27731cf14a644f726df0c
Danylo <zdanilevich@gmail.com>
2023-08-12 17:32:52 +0300

    Backend basic useabilities. Working on front

--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router()
 const basketController = require('../controllers/basketController.js')
 
-router.post('/add-to-basket')
-router.get('/')
+router.post('/add-to-basket')// add
+router.get('/')// get basket 
 
 
 module.exports = router
